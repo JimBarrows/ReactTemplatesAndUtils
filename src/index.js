3d3cabe85170486096763ffebb56185b78ce2f22
Jim Barrows <jim@reallybigtree.com>
2016-12-18 21:57:47 -0700

    Point to the correct directories.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
  * Created by JimBarrows on 12/18/16.
  */
 
-export {ItemDescription, ItemEditor, ItemList} from "Item";
-export {checkHttpStatus, parseJSON, convertErrorToString, createConstants, createReducer} from "utils";
+export {ItemDescription, ItemEditor, ItemList} from "./Item";
+export {checkHttpStatus, parseJSON, convertErrorToString, createConstants, createReducer} from "./utils";
